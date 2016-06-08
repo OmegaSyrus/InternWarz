927d52a5c2b2bc8ce50602591bb67b41e9ae5b53
OmegaSyrus <omega_syrus@hotmail.com>
2016-06-07 23:59:55 -0700

    initial commit
    First commit contains rudimentary working facebook login button in the
    login page using "bootstrap social buttons"
    
    This project was originally generated from a Visual Studio 2015 ASP.NET
    Core template

---
+++ src/InternWarz/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Write your Javascript code.
+$(function () {
+    $("#FacebookLogin").on("click", function (e) {
+        e.preventDefault(); // cancel the link itself
+        $.ajax({
+            type: "POST",
+            url: this.href,
+            data: { provider: "Facebook" },
+            contentType: "application/json",
+            success: () => { },
+            dataType: () => { }
+        });
+        $.post(this.href, {provider: "Facebook"}, function (data) {
+            $("#FacebookLogin").html(data);
+        });
+    });
+});
+
